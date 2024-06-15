6de2bcf834c05060ab70b3961797ba60df148eb8
Luis Vargas <vargasluis012@gmail.com>
2024-06-15 00:20:37 -0400

    Added settings but still doesn't seem to lint Typescript files.

--- after/plugin/sonar-lint.lua
+++ after/plugin/sonar-lint.lua
@@ -8,15 +8,25 @@ require('sonarlint').setup({
 			-- paths to the analyzers you need, using those for python and java in this example
 			vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
 			vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
-			vim.fn.expand("$MASON/share/sonarlint-analyzers/sonartypescript.jar"),
-			vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjavascript.jar"),
+			vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarhtml.jar"),
+			vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjs.jar"),
 		},
+		settings = {
+			-- The default for sonarlint is {}, this is just an example
+			sonarlint = {
+				rules = {
+					['typescript:S101'] = { level = 'on', parameters = { format = '^[A-Z][a-zA-Z0-9]*$' } },
+					['typescript:S103'] = { level = 'on', parameters = { maximumLineLength = 180 } },
+					['typescript:S106'] = { level = 'on' },
+					['typescript:S107'] = { level = 'on', parameters = { maximumFunctionParameters = 7 } }
+				}
+			}
+		}
 	},
 	filetypes = {
 		'javascript', 'javascriptreact', 'javascript.jsx',
 		'typescript', 'typescriptreact', 'typescript.tsx',
-		'vue', 'svelte', 'astro'
+		'vue', 'svelte', 'astro', 'ts', 'js'
 	},
 	autostart = true,
-	settings = {},
 })
