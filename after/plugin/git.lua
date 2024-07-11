4202b70688824cc060a50c1fc105a683857b7ebd
Luis Vargas <vargasluis012@gmail.com>
2024-07-11 13:34:07 -0400

    Fixed after version bump.

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,11 +1,21 @@
 require('gitsigns').setup {
 	signs = {
-		add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-		change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-		delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		add          = { text = '▎' },
+		change       = { text = '▎' },
+		delete       = { text = '_' },
+		topdelete    = { text = '‾' },
+		changedelete = { text = '~' },
+		untracked    = { text = '┆' },
 	},
+	signs_staged = {
+		add          = { text = '▎' },
+		change       = { text = '▎' },
+		delete       = { text = '_' },
+		topdelete    = { text = '‾' },
+		changedelete = { text = '~' },
+		untracked    = { text = '┆' },
+	},
+	signs_staged_enable = true,
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
@@ -17,12 +27,6 @@ require('gitsigns').setup {
 	auto_attach = true,
 	attach_to_untracked = true,
 	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-	current_line_blame_opts = {
-		virt_text = true,
-		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-		delay = 1000,
-		ignore_whitespace = false,
-	},
 	current_line_blame_formatter_opts = {
 		relative_time = false,
 	},
@@ -37,9 +41,6 @@ require('gitsigns').setup {
 		relative = "cursor",
 		row = 0,
 		col = 1,
-	},
-	yadm = {
-		enable = false,
 	},
 	on_attach = function(bufnr)
 		local gitsigns = require('gitsigns')
