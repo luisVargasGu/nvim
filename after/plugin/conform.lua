c45207c9b29a6e9e6f0f78bbe829ae23cbf57bcb
Luis Vargas <vargasluis012@gmail.com>
2025-01-14 08:35:11 -0500

    Formatting.

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,11 +1,23 @@
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
+		go = { "gofmt" },
 		-- Conform will run multiple formatters sequentially
 		python = { "isort", "black" },
+		terraform = { "terraform_fmt" },
+		tf = { "terraform_fmt" },
+		yaml = { "yamlfmt" },
+		yml = { "yamlfmt" },
+		c = { "clang-format" },
 		-- Use a sub-list to run only the first available formatter
 		javascript = { "prettier" },
+		typescript = { "prettier" },
+		html = { "prettier" },
+		css = { "prettier" },
+		scss = { "prettier" },
+		sass = { "prettier" },
 		json = { "prettier" },
+		jsonc = { "prettier" },
 		javascriptreact = { "prettier" },
 		typescriptreact = { "prettier" },
 	},
